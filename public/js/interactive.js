9ae5a7e505ccb73d44a8ab6fcc4f045c059a9269
darksim735 <darksim735@gmail.com>
2015-03-17 21:18:16 -0400

    gets primary button for first hotel section but not the ones below

---
+++ public/js/interactive.js
@@ -0,0 +1,11 @@
+alert("hello");
+
+// button functionality for hotels
+$(".btn-primary").click(function(){
+   alert("button clicked");
+   // print selection
+   var selection = $("select").val();
+   console.log(selection);
+});
+
+
