ff94bea6e76f6689faa015fb76f546c6dc794811
darksim735 <darksim735@gmail.com>
2015-03-19 19:04:36 -0400

    access correct category in itinerary for restaurant, things, etc instead of just the first to appear

--- public/js/interactive.js
+++ public/js/interactive.js
@@ -4,6 +4,24 @@ alert("hello");
 $(".btn-primary").click(function(){
    alert("button clicked");
    // print selection
+   var selection = $(this).parent().find('select').val();
+   console.log(selection);
+
+//   var parent = $(this).parent();
+//   console.log(parent);
+   
+//   var h4 = $(this).prev().prev();
+//   console.log(h4);
+  
+//   var h4val = $(this).prev().prev().val();
+//   console.log(h4val); 
+
+});
+
+// button functionality for hotels
+$(".btn-danger").click(function(){
+   alert("button clicked, remove element");
+   // print selection
    var selection = $("select").val();
    console.log(selection);
 });
